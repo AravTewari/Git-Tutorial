3365c33507fbb97b8a087241eb8315a352e1c665
Arav Tewari <aravtewari@gmail.com>
2021-03-09 14:57:32 -0800

    Initial commit

---
+++ app/src/main/java/com/example/gittutorial/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.gittutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+// TODO: Add a comment with your name and push it to the main branch
+// Arav Tewari
